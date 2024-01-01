8f8af5df2a3c4626e39b42d0d802d8f44fade0d9
Osmilliardo <140178301+Osmilliardo@users.noreply.github.com>
2024-01-01 15:37:14 -0500

    added prompts

--- db/schema.sql
+++ db/schema.sql
@@ -20,10 +20,12 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id),
+    REFERENCES role(id),
     manager_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    FOREIGN KEY (manager_id),
+    REFERENCES employee(id)    
 );
